2b8959f1c1a5a9189388f5b8222225d4e5017eb4
Sebastian Eberlein <sebastian@juno-hamburg.com>
2023-12-01 14:56:35 +0100

    Add Kirby version detection

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
 
+use Kirby\Cms\App as Kirby;
+
+// Validate Kirby version
+if (
+  version_compare(Kirby::version() ?? '0.0.0', '3.6.0', '<') === true ||
+  version_compare(Kirby::version() ?? '0.0.0', '3.10.0', '>=') === true
+) {
+  throw new Exception('The block preview fields plugin supports Kirby 3.6.0 to 3.9.x');
+}
+
 Kirby::plugin('junohamburg/visual-block-selector', [
   'api' => [
     'routes' => [
