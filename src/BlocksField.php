bd96f7ac5c1b0b4d598e1f2bd65c62a3769c32f2
Bastian Allgeier <mail@bastianallgeier.com>
2025-05-21 10:55:55 +0200

    Add a custom BlocksField class

---
+++ src/BlocksField.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace JunoHamburg\VisualBlockSelector;
+
+use Kirby\Filesystem\Dir;
+use Kirby\Form\Field\BlocksField as KirbyBlocksField;
+
+/**
+ * Custom BlocksField class, which provides the
+ * visual block selector component with preview images.
+ */
+class BlocksField extends KirbyBlocksField
+{
+	/**
+	 * Collects all block preview images
+	 * in the assets directory
+	 *
+	 * @return array<string, string>
+	 */
+	protected function previews(): array
+	{
+		$kirby    = kirby();
+		$previews = [];
+		$dir      = 'block-previews';
+		$path     = $kirby->root('assets') . '/' . $dir;
+		$images   = Dir::files($path);
+
+		$relativePath = str_replace($kirby->root('index') . '/', '', $kirby->root('assets'));
+
+		foreach ($images as $image) {
+			$asset = asset($relativePath . '/' . $dir . '/' . $image);
+			$previews[$asset->name()] = $asset->url();
+		}
+
+		return $previews;
+	}
+
+	/**
+	 * Adds the preview images to the fieldsets
+	 * if they exist
+	 */
+	public function props(): array
+	{
+		$props    = parent::props();
+		$previews = $this->previews();
+
+		$props['fieldsets'] = array_map(function (array $fieldset) use ($previews) {
+			$fieldset['preview'] = $previews[$fieldset['type']] ?? null;
+			return $fieldset;
+		}, $props['fieldsets']);
+
+		return $props;
+	}
+}
