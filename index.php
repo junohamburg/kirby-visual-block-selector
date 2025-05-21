408dd73183964dd11d2c6db41451b2db51a109c8
Bastian Allgeier <mail@bastianallgeier.com>
2025-05-21 10:56:24 +0200

    Auto-load the custom class and remove the API endpoint

--- index.php
+++ index.php
@@ -5,30 +5,16 @@ use Composer\Semver\Semver;
 
 // Validate Kirby version
 if (Semver::satisfies(Kirby::version() ?? '0.0.0', '~4.0 || ~5.0') === false) {
-  throw new Exception('The visual block selector plugin requires Kirby 4');
+	throw new Exception('The visual block selector plugin requires Kirby 4');
 }
 
+// Auto-load the custom BlocksField class
+load([
+	'JunoHamburg\VisualBlockSelector\BlocksField' => __DIR__ . '/src/BlocksField.php',
+]);
+
 Kirby::plugin('junohamburg/visual-block-selector', [
-  'api' => [
-    'routes' => [
-      [
-        'pattern' => 'visual-block-selector',
-        'action'  => function () {
-          $previewImages = [];
-          $dir = 'block-previews';
-          $path = kirby()->root('assets') . '/' . $dir;
-          $imageFiles = Dir::files($path);
-
-          $relativePath = str_replace(kirby()->root('index') . '/', '', kirby()->root('assets'));
-
-          foreach ($imageFiles as $image) {
-            $imageAsset = asset($relativePath . '/' . $dir . '/' . $image);
-            $previewImages[$imageAsset->name()] = $imageAsset->url();
-          }
-
-          return $previewImages;
-        }
-      ]
-    ]
-  ]
+	'fields' => [
+		'blocks' => 'JunoHamburg\VisualBlockSelector\BlocksField'
+	],
 ]);
